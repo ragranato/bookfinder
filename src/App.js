cb61a4f16f6163f99a74fa2bf2a61ad505502267
ragranato <robert.granato@gmail.com>
2023-07-05 22:18:32 -0400

    added genre selector, banner, and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './App.styles.scss';
 
 function App() {
+  const [ selection, setSelection ] = useState('');
+
+  useEffect(() => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${selection}&key=API`);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='book-selector'>
+        <h1>Find Your Next Book</h1>
+        <form className="genre-form">
+          <div>
+            <label className="genre-label">Choose a Genre</label>
+            <select className="selections">
+              <option>Select One</option>
+              <option value="mystery">Mystery</option>
+              <option value="fantasy">Fantasy</option>
+              <option value="science fiction">Science Fiction</option>
+              <option value="romance">Romance</option>
+              <option value="horror">Horror</option>
+            </select>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
